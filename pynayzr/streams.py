eeb6069942b22ae49bf3097ae922c431a864beed
Louie Lu <git@louie.lu>
2019-04-01 16:29:36 +0800

    stream: TVBS change link to youtube

--- pynayzr/streams.py
+++ pynayzr/streams.py
@@ -14,32 +14,8 @@ support_news = {
     'ftv': 'https://www.youtube.com/watch?v=XxJKnDLYZz4',
     'ctv': 'https://www.youtube.com/watch?v=qIa-L8URVBI',
     'cts': 'https://www.youtube.com/watch?v=7UF21-RF0pY',
-    'tvbs': 'https://bcsecurelivehls-i.akamaihd.net/hls/live/510708/4862438529001/playlistTVBS-N-HD-CH1-M.m3u8'
+    'tvbs': 'https://www.youtube.com/watch?v=Hu1FkdAOws0'
 }
-
-
-def tvbs():
-    """Return TVBS livestream frame
-    # NOTE: Not sure tvbs link will expire or not
-    """
-    with tempfile.TemporaryDirectory() as temp_dir:
-        ffmpeg = [
-            'ffmpeg',
-            '-i',
-            support_news['tvbs'],
-            '-f',
-            'image2',
-            '-vframes',
-            '1',
-            '%s/out.jpg' % (temp_dir)
-        ]
-
-        p2 = subprocess.Popen(ffmpeg,
-                              stdout=subprocess.DEVNULL,
-                              stderr=subprocess.DEVNULL)
-        p2.communicate()
-
-        return Image.open('%s/out.jpg' % (temp_dir))
 
 
 def get(news):
@@ -52,10 +28,6 @@ def get(news):
     """
     if news not in support_news:
         raise KeyError
-
-    # TVBS not using youtube
-    if news.startswith('tvbs'):
-        return tvbs()
 
     # Other news using youtube
     with tempfile.TemporaryDirectory() as temp_dir:
